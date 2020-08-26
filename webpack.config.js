b42ef1339f8cdf31c7c5f1888b4e41c131817d69
SeanCho <chunghwan14@gmail.com>
2020-08-26 17:03:56 -0400

    webpack setup with plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
     entry: './src/js/index.js',
 
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
-    }
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    
+    //Injecting index.html from src
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
 
+    ]
 };
