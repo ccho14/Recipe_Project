c205b4ed214f08bfda818a5135d34da8af27aac5
Sean <chunghwan14@gmail.com>
2020-09-30 22:30:21 -0400

    Recipe title with ...

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -4,13 +4,34 @@ import {
 
 export const getInput = () => elements.searchInput.value;
 
+//Clears input field
 export const clearInput = () => {
     elements.searchInput.value = '';
 };
 
+//Clears rendered result
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
-}
+};
+
+//Spliting the string
+//accumulating until limit algorithm
+
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+
+        //return the result
+        return `${newTitle.join(' ')} ...`
+    }
+    return title;
+};
 
 const renderRecipe = recipe => {
     const markup = `
@@ -20,13 +41,13 @@ const renderRecipe = recipe => {
             <img src="${recipe.image_url}" alt="${recipe.title}">
         </figure>
         <div class="results__data">
-            <h4 class="results__name">${recipe.title}</h4>
+            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
             <p class="results__author">${recipe.publisher}</p>
         </div>
     </a>
 </li> 
    `;
-   elements.searchResList.insertAdjacentHTML('beforeend', markup);
+    elements.searchResList.insertAdjacentHTML('beforeend', markup);
 };
 
 export const renderResults = recipes => {
